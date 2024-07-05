1cf892b41171dd838237a1c0c59b30a200124f05
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2024-07-05 17:02:51 +0200

    Adding vehicle class

---
+++ src/main/java/com/mycompany/app/easy/parking_lot/vechicles_type/Car.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.easy.parking_lot.vechicles_type;
+
+
+public class Car extends Vehicle {
+    public Car(String plate) {
+        super(plate, VehiclesType.CAR);
+    }
+}
---
+++ src/main/java/com/mycompany/app/easy/parking_lot/vechicles_type/Motorcycle.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.easy.parking_lot.vechicles_type;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(String plate) {
+        super(plate, VehiclesType.MOTORCYCLE);
+    }
+}
