3fd0ed23bdb23117e4c0deaccc1d3976d777e40a
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2024-07-11 17:11:53 +0200

    Adding main & readme files

---
+++ src/main/java/com/mycompany/app/hard/ride_sharing_service/Main.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.hard.ride_sharing_service;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        System.out.println("Hello world!");
+    } 
+}
